639c309c5a268639acc8bbc43bd4623ebc928d02
SenanM1 <senan.mardanli@gmail.com>
2022-03-23 00:23:17 +0400

    Updates

---
+++ precache-manifest.4788853ebf82ff7ca9d289239b71c730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfddc0fa85868e4f28e4",
+    "url": "/FirstReactProject/static/css/main.7fcc942d.chunk.css"
+  },
+  {
+    "revision": "bfddc0fa85868e4f28e4",
+    "url": "/FirstReactProject/static/js/main.03c2c0e7.chunk.js"
+  },
+  {
+    "revision": "637cbebcde17f3ca317f",
+    "url": "/FirstReactProject/static/js/runtime~main.0bb8733f.js"
+  },
+  {
+    "revision": "3fe5ccb45398a7be9e86",
+    "url": "/FirstReactProject/static/js/2.f8ba827f.chunk.js"
+  },
+  {
+    "revision": "bf69a875b24b00e886ff6be8d37accb7",
+    "url": "/FirstReactProject/index.html"
+  }
+];
